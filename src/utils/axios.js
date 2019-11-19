c3273c76f4233e028190b2469acac34e97f26ab3
Rosy <rosyshrestha028@gmail.com>
2019-11-19 17:54:45 +0545

    [AXIOS] : Create axios util

---
+++ src/utils/axios.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const instance = axios.create({
+    baseURL: process.env.REACT_APP_BASE_URL,
+    timeout: 0
+})
+
+export default instance;
+
+// axios.get(`/gifs/search?q=smile&api_key=${process.env.REACT_APP_API_KEY}`)
